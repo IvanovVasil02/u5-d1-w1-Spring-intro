b2fa883f35008fd30269db8d4e5c7907d2defdc9
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-10-30 17:06:40 +0100

    Add folder

---
+++ src/test/java/ivanovvasil/u5d1w1/U5D1W1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ivanovvasil.u5d1w1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5D1W1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
